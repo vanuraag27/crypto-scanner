4b235f8d7864c5b20a3c9aadb629f319ff5dc867
vanuraag27 <a297020625@gmail.com>
2025-09-08 11:03:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,13 @@ module.exports = {
   ALERT_UP_THRESHOLD: 10,        // % gain to alert
   ALERT_DOWN_THRESHOLD: -10,     // % drop to alert
   USE_TELEGRAM: true,            // enable Telegram alerts
-  TELEGRAM_TOKEN: "8021807728:AAEfBJTy_znD0YarO8Mm7O_YdHDEnZx7q-M",  // replace with your bot token
-  CHAT_ID: "1783057190",           // replace with your chat ID
+  BOT_TOKEN=8021807728:AAEfBJTy_znD0YarO8Mm7O_YdHDEnZx7q-M
+  CHAT_ID=1783057190
   REFRESH_INTERVAL: 30 * 1000, // 30 seconds
   MIN_VOLUME: 500000, // Only scan coins with volume > $500k
- FAST_DELTA_THRESHOLD: 0.5, // Only alert if coin moves more than $0.5 since last scan
+  FAST_DELTA_THRESHOLD: 0.5, // Only alert if coin moves more than $0.5 since last scan
   ALERT_10_PERCENT_THRESHOLD: 10, // Only alert if 24h change ≥ 10%
   ALERT_20_PERCENT_THRESHOLD: 20,  // Only alert if 24h change ≥ 20%
   PREDICTION_TOP_N: 20 // Number of coins to show predicted gain/loss
 };
+
