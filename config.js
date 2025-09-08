5eb2f7dca3e42729443587d65c7ec8bbeb3e5afb
vanuraag27 <a297020625@gmail.com>
2025-09-08 13:14:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  REFRESH_INTERVAL: process.env.REFRESH_INTERVAL || 300000, // default 5 minutes
-  USE_TELEGRAM: process.env.USE_TELEGRAM !== "false",       // default true unless explicitly set to "false"
+  REFRESH_INTERVAL: process.env.REFRESH_INTERVAL || 300000, // 5 minutes
+  USE_TELEGRAM: process.env.USE_TELEGRAM !== "false", // default true
   BOT_TOKEN: process.env.BOT_TOKEN || process.env.TELEGRAM_BOT_TOKEN,
   CHAT_ID: process.env.CHAT_ID || process.env.TELEGRAM_CHAT_ID,
   CMC_API_KEY: process.env.CMC_API_KEY
