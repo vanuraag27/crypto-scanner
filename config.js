428f7551a29677a2ef2a5247a5b0d462a7efbb5b
vanuraag27 <a297020625@gmail.com>
2025-09-08 12:09:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,5 @@ module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   CHAT_ID: process.env.CHAT_ID,
   CMC_API_KEY: process.env.CMC_API_KEY // NEW
-  MIN_VOLUME: 500000,
-  FAST_DELTA_THRESHOLD: 0.5,
-  ALERT_10_PERCENT_THRESHOLD: 10,
-  ALERT_20_PERCENT_THRESHOLD: 20,
-  PREDICTION_TOP_N: 20
-};
+  };
 
