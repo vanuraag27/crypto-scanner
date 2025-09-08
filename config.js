bee99cdd1fe069bd326388dd9ed32133c49986f0
vanuraag27 <a297020625@gmail.com>
2025-09-08 12:51:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  REFRESH_INTERVAL: 10 * 60 * 1000, // every 10 min (safe for free tier)
-  ALERT_UP_THRESHOLD: 10,        // % gain alert
-  ALERT_DOWN_THRESHOLD: -10,     // % drop alert
+  REFRESH_INTERVAL: 300000, // every 10 min (safe for free tier)
   USE_TELEGRAM: true,            // enable Telegram
   BOT_TOKEN: process.env.BOT_TOKEN,
   CHAT_ID: process.env.CHAT_ID,
