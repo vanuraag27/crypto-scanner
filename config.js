d4b978602ad00eaab886aaf9d70e7c77b8e59418
vanuraag27 <a297020625@gmail.com>
2025-09-09 11:41:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+```javascript
 module.exports = {
   BOT_TOKEN: process.env.TELEGRAM_TOKEN || '',
   CHAT_ID: process.env.CHAT_ID || '',
@@ -6,3 +7,4 @@ module.exports = {
   ALERT_10_PERCENT_THRESHOLD: parseInt(process.env.ALERT_10_PERCENT_THRESHOLD, 10) || 10,
   REFRESH_INTERVAL: parseInt(process.env.REFRESH_INTERVAL, 10) || 120000 // 2 minutes
 };
+```
