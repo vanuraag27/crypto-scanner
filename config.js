1e301e39c81223d286953154cab8708caddf5093
vanuraag27 <a297020625@gmail.com>
2025-09-10 10:16:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// config.js
+require("dotenv").config();
+
 module.exports = {
   BOT_TOKEN: process.env.TELEGRAM_TOKEN || "",
   CHAT_ID: process.env.CHAT_ID || "",
   CMC_API_KEY: process.env.CMC_API_KEY || "",
-  USE_TELEGRAM: process.env.USE_TELEGRAM === "true",
-  REFRESH_INTERVAL: process.env.REFRESH_INTERVAL
-    ? parseInt(process.env.REFRESH_INTERVAL)
-    : 600000 // default 10 min
+  REFRESH_INTERVAL: parseInt(process.env.REFRESH_INTERVAL || "600000", 10), // 10 min default
+  USE_TELEGRAM: process.env.USE_TELEGRAM === "true"
 };
