25b0e4d029450b85e6fc3eb6983a258becf86df0
vanuraag27 <a297020625@gmail.com>
2025-09-11 10:48:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   CHAT_ID: process.env.CHAT_ID || "",
   CMC_API_KEY: process.env.CMC_API_KEY || "",
   USE_TELEGRAM: process.env.USE_TELEGRAM === "true",
-  REFRESH_INTERVAL: parseInt(process.env.REFRESH_INTERVAL) || 10 * 60 * 1000, // default 10 min
   PORT: process.env.PORT || 10000,
-  BASE_URL: process.env.BASE_URL || "https://crypto-scanner-jaez.onrender.com"
+  BASE_URL: process.env.BASE_URL || "https://crypto-scanner-jaez.onrender.com",
+  REFRESH_INTERVAL: 5 * 60 * 1000 // check alerts every 5 minutes
 };
