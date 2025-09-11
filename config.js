dce1e18ea27d7c80196acbb15245d16ae979bace
vanuraag27 <a297020625@gmail.com>
2025-09-11 21:50:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  PORT: process.env.PORT || 10000,
+  BOT_TOKEN: process.env.TELEGRAM_TOKEN || "YOUR_TELEGRAM_BOT_TOKEN",
+  CHAT_ID: process.env.CHAT_ID || "YOUR_CHAT_ID",
+  ADMIN_USER: process.env.ADMIN_USER || "anurag999", // set your Telegram username
+  CMC_API_KEY: process.env.CMC_API_KEY || "YOUR_CMC_API_KEY",
   BASE_URL: process.env.BASE_URL || "https://crypto-scanner-jaez.onrender.com",
-  BOT_TOKEN: process.env.TELEGRAM_TOKEN || "your-telegram-bot-token",
-  CHAT_ID: process.env.CHAT_ID || "your-chat-id",
-  CMC_API_KEY: process.env.CMC_API_KEY || "your-cmc-api-key",
-  ADMINS: [1783057190] // Replace with your Telegram user ID(s)
+  PORT: process.env.PORT || 10000
 };
